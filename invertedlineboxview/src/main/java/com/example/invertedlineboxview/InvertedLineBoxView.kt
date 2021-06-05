07d3497c8495ab8eb3012c870e2dea0b91814315
Anwesh43 <anweshthecool0@gmail.com>
2021-06-06 03:13:10 +0530

    adding InvertedLineBox class

--- invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
+++ invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
@@ -166,6 +166,28 @@ class InvertedLineBoxView(ctx : Context) : View(ctx) {
             cb()
             return this
         }
+    }
 
+    data class InvertedLineBox(var i : Int) {
+
+        private var curr : ILBNode = ILBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
     }
 }
