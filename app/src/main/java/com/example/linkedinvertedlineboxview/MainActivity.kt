3b5661d0766a77d818cd11c4e762d5e7f90cb718
Anwesh43 <anweshthecool0@gmail.com>
2021-06-06 03:20:56 +0530

    adding static create method

--- app/src/main/java/com/example/linkedinvertedlineboxview/MainActivity.kt
+++ app/src/main/java/com/example/linkedinvertedlineboxview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedinvertedlineboxview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.invertedlineboxview.InvertedLineBoxView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        InvertedLineBoxView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
