b5ff95e0946f749da385c1f40eed237566707f22
Anwesh43 <anweshthecool0@gmail.com>
2021-06-06 02:43:31 +0530

    adding InvertedLineBoxView class

--- invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
+++ invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawILBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawInvertedLineBox(scale, w, h, paint)
 }
+
+class InvertedLineBoxView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canva : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
