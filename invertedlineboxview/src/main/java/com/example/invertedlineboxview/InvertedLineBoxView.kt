5f308ce281ff87a54b5d777165c10beb7112716c
Anwesh43 <anweshthecool0@gmail.com>
2021-06-06 03:18:42 +0530

    adding static create method

--- invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
+++ invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
@@ -215,4 +215,13 @@ class InvertedLineBoxView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : InvertedLineBoxView {
+            val view : InvertedLineBoxView = InvertedLineBoxView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
