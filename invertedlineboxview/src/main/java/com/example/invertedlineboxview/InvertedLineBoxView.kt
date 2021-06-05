460a289ce0904fd3a0575dc38a3ccccfb15bffaf
Anwesh43 <anweshthecool0@gmail.com>
2021-06-06 03:10:56 +0530

    adding ILBNode class

--- invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
+++ invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
@@ -126,4 +126,46 @@ class InvertedLineBoxView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ILBNode(var i : Int, val state : State = State()) {
+
+        private var next : ILBNode? = null
+        private var prev : ILBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ILBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawILBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ILBNode {
+            var curr : ILBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
