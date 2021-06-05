105362d66a23fa64165c4b4a755a7bf21ec69bd8
Anwesh43 <anweshthecool0@gmail.com>
2021-06-06 03:16:22 +0530

    adding renderer in view

--- invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
+++ invertedlineboxview/src/main/java/com/example/invertedlineboxview/InvertedLineBoxView.kt
@@ -66,14 +66,16 @@ fun Canvas.drawILBNode(i : Int, scale : Float, paint : Paint) {
 
 class InvertedLineBoxView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canva : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
